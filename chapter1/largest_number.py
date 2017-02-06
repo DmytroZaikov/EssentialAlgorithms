fc5efabb637d1c1c6fbd6f44c9f7dc3b0bda4111
DZaikov <dmytro.zaikov@gmail.com>
2017-02-06 21:56:58 +0100

    Added big O complexity to largest numbers

--- chapter1/largest_number.py
+++ chapter1/largest_number.py
@@ -2,11 +2,12 @@
 
 
 def largest_number(lst):
-    max_number = lst[0]
+    max_number = lst[0]  # O(1)
     for elem in lst:
-        if elem > max_number:
+        if elem > max_number:  # O(N)
             max_number = elem
-    return max_number
+    return max_number  # O(1)
+
 
 a = [1, 3, 4, 3, 8, 2]
 print(largest_number(a))
