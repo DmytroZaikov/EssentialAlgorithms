261e69085db1df935ec3aac4ce10363f06cc2550
DZaikov <dmytro.zaikov@gmail.com>
2017-02-07 22:30:12 +0100

    Added comment with algorithm description

--- chapter1/gcd.py
+++ chapter1/gcd.py
@@ -1,6 +1,13 @@
 # Find the greatest common divisor of a and b.
 # GCD(a, b) = GCD(b, a Mod b).
 
+# One way to find the GCD is to factor the two numbers
+# and see which factors they have in common.
+# However, the Greek mathematician Euclid recorded a faster method
+# in his treatise Elements circa 300 BC. Because it is based on Euclid's work
+# this algorithm is called the Euclidian algorithm or Euclid's algorithm:
+
+
 def gcd(a, b):
     while b != 0:
         reminder = a % b
